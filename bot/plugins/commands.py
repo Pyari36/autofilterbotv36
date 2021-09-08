dba8d4bfa6bced6d87f56c25d97731140e5bae7d
Pyari36 <88506599+Pyari36@users.noreply.github.com>
2021-09-08 10:46:12 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @moviesmaniapyari,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰Our Group🔰', url="https://t.me/moviemaniapyari"
                                 )
                         ]
                     ]
